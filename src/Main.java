f84388b92b7ee1b3398ffa5b099efce7a0ac4f20
Jana C <janakatharina.cording@posteo.de>
2020-05-30 21:45:11 +0200

    modify user prompts

--- src/Main.java
+++ src/Main.java
@@ -13,15 +13,27 @@ public class Main {
 	 * @throws IOException
 	 */
 	public static void main(String args[]) throws IOException {
+
+		System.out.println("This app encrypts as well as decrypts data.\n");
+
+		System.out.println("You will now be prompted to enter parameters used to encrypt or decrypt your file data.");
+
+		System.out.println(
+				"In order to decrypt a file that has previously been encrypted with a set of parameters, the same parameters should be used.");
+
+		System.out.println(
+				"As encryption quality depends significantly on the complexity of your parameters, if you'd like to give it a quick try, here's my recommendation:\n");
+
+		System.out.println("a : 421");
+
+		System.out.println("b : 54,773");
+
+		System.out.println("m : 259,200");
+
+		System.out.println("key : e.g. 3\n");
 		
-		System.out.println("Hallo! Falls Sie diese Datei ausführen möchten, habe ich die Beispieldatei Gedicht.txt beigefügt.");
-		System.out.println("Die Qualität einer Chiffrierung ist maßgeblich von den gewählten Parametern abhängig.");
-		System.out.println("Wenn Sie möchten, könnten Sie diese hier nutzen:");
-		System.out.println("a: 421");
-		System.out.println("b: 54,773");
-		System.out.println("m: 259,200");
-		System.out.println("Schlüssel: z.B. 3");
-		System.out.println("Um eine chiffrierte Datei zu dechiffrieren, geben Sie diese Parameter einfach erneut ein.\n");
+		System.out.println("As a sample file, I've included 'Gedicht.txt'.");
+		System.out.println("The program will now be executed :-).\n");
 
 		Chiffrierung chiff = new Chiffrierung();
 		chiff.execute();
